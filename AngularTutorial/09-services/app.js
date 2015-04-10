c01f883a99be9bdf07650d6b7f0b34bd452f3a19
Daniel <danielbeckmann@live.de>
2015-04-10 14:52:40 +0200

    Added 09 and 10 and began 11

---
+++ AngularTutorial/09-services/app.js
@@ -0,0 +1,26 @@
+﻿(function () {
+    'use strict';
+
+    var app = angular.module('moviesApp', []);
+
+    app.controller('MoviesCtrl', ['$http',
+        function ($http) {
+            var vm = this;
+            vm.movies = [];
+
+            $http.get('movies.json').then(function (response) {
+                vm.movies = response.data;
+            });
+    }]);
+
+    app.filter('rating', function () {
+        return function (rating) {
+            var output = '';
+
+            for (var i = 0; i < rating; i++) output += '\u2605';
+            for (var j = rating; j < 5; j++) output += '\u2606';
+
+            return output;
+        };
+    });
+}());
---
+++ AngularTutorial/11-routing/app.js
@@ -0,0 +1,39 @@
+﻿(function () {
+    'use strict';
+
+    var app = angular.module('moviesApp', []);
+
+    app.controller('MoviesCtrl', ['movieService',
+        function (service) {
+            var vm = this;
+            vm.movies = [];
+
+            service.getPopular().then(function (response) {
+                vm.movies = response.data;
+            });
+    }]);
+
+    app.factory('movieService', ['$http',
+        function ($http) {
+            var serviceUrl = 'movies.json';
+
+            var getPopular = function () {
+                return $http.get(serviceUrl);
+            };
+
+            return {
+                getPopular: getPopular
+            };
+    }]);
+
+    app.filter('rating', function () {
+        return function (rating) {
+            var output = '';
+
+            for (var i = 0; i < rating; i++) output += '\u2605';
+            for (var j = rating; j < 5; j++) output += '\u2606';
+
+            return output;
+        };
+    });
+}());
